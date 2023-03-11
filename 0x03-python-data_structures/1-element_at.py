d72f19a916ca53c4ec65ceffa48d7a3a7bee849b
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-03-10 16:41:43 -0800

    tset

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx > len(mylist) or idx < 0:
-        return None
-    return my_list[idx - 1]
+        return (None)
+    return (my_list[idx - 1])
